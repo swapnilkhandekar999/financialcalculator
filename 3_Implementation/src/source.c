715ae9246747f426aeda3ee82b324ef21a1df00c
swapnilkhandekar999 <swapnilkhandekar999@gmail.com>
2021-04-13 21:38:02 +0530

    Updated test

--- 3_Implementation/src/source.c
+++ 3_Implementation/src/source.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-long double gratuity_calculation(long double P, int n, int m,int e){
-    long double result=0;
+#include<math.h>
+
+long int gratuity_calculation(long double P, int n, int m,int e){
+    long int result=0;
     if(number_of_months>=5)n++;
+    if(n<5){
+        printf("\nTo be eligible for gratuity under the Gratuity Act,\n an employee needs to have at least five full years of service\n with the current employer, except in the event that an employee\n passes away or is rendered disabled due to accident or illness.\n In these cases, gratuity must be paid.\n");
+    }
     if(eligibility==1){
         result = P*(15/26)*n;
         return result;
@@ -13,17 +18,17 @@ long double gratuity_calculation(long double P, int n, int m,int e){
     else printf("\nPlease try again");
 }
 
-long double fixed_deposit(long double P,int n, float r){
-    long double result = P*((1+r/100)*n);
+long int fixed_deposit(long double P,int n, float r){
+    long int result = P*(pow((1+r/100),n));
     return result;
 }
 
-long double roi(long double P,int n, float r){
-    long double result = P*((1+r/100)*n)-P;
+long int roi(long double P,int n, float r){
+    long int result = P*(pow((1+r/100),n))-P;
     return result;
 }
 
-long double emi(long double P,int n, float r){
-    long double result = P*r*(1+r)*n/((1+r)*n-1);
+long int emi(long double P,int n, float r){
+    long int result = P*r*(1+r)*n/((1+r)*n-1);
     return result;
 }
--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -33,17 +33,22 @@ int main()
 
 /* Write all the test functions */ 
 void test_gratuity(void) {
-  TEST_ASSERT_EQUAL(30, gratuity_calculation(30000,5,5,1));
-  
+  TEST_ASSERT_EQUAL(10384, gratuity_calculation(3000,5,5,1));
+  TEST_ASSERT_EQUAL(103846, gratuity_calculation(30000,5,5,1));
+  TEST_ASSERT_EQUAL(1038461, gratuity_calculation(300000,5,5,1));
+  TEST_ASSERT_EQUAL(10384615, gratuity_calculation(3000000,5,5,1));
+  TEST_ASSERT_EQUAL(0, gratuity_calculation(0,5,5,1));
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(15000, gratuity_calculation(30000,5,5,1));
+  //TEST_ASSERT_EQUAL(15000, gratuity_calculation(30000,5,5,1));
 }
 
 void test_fixed_deposit(void) {
-  TEST_ASSERT_EQUAL(-3, fixed_deposit(100000,3,6.65));
+  TEST_ASSERT_EQUAL(121306, fixed_deposit(100000,3,6.65));
+  TEST_ASSERT_EQUAL(0, fixed_deposit(0,3,6.65));
+  TEST_ASSERT_EQUAL(121306, fixed_deposit(1000000,10,10));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(100, fixed_deposit(100000,3,6.65));
+  //TEST_ASSERT_EQUAL(100, fixed_deposit(100000,3,6.65));
 }
 
 void test_roi(void) {
