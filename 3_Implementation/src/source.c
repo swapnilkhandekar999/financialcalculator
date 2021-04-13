4fd7bed8aaed984bbb9e8e71fb1cdb35966b1b20
swapnilkhandekar999 <swapnilkhandekar999@gmail.com>
2021-04-13 20:22:57 +0530

    Updated definitions

--- 3_Implementation/src/source.c
+++ 3_Implementation/src/source.c
@@ -1,19 +1,29 @@
 #include<stdio.h>
-long double gratuity_calculation(long double amount, int number_of_years, int number_of_months,int eligibility){
-    if(number_of_months>=5)number_of_years++;
-    if(eligibility==1)return amount * (15/26) * number_of_years;
-    else if(eligibility==0)return amount * (15/30) * number_of_years;
+long double gratuity_calculation(long double P, int n, int m,int e){
+    long double result=0;
+    if(number_of_months>=5)n++;
+    if(eligibility==1){
+        result = P*(15/26)*n;
+        return result;
+    }
+    else if(eligibility==0){
+        result = P*(15/30)*n;
+        return result;
+    }
     else printf("\nPlease try again");
 }
 
-long double fixed_deposit(long double amount,int number_of_years, float rate_of_interest){
-    return amount * ((1 + rate_of_interest/100) * number_of_years);
+long double fixed_deposit(long double P,int n, float r){
+    long double result = P*((1+r/100)*n);
+    return result;
 }
 
-long double roi(long double amount,int number_of_years, float rate_of_interest){
-    return amount * ((1 + rate_of_interest/100) * number_of_years) - amount;
+long double roi(long double P,int n, float r){
+    long double result = P*((1+r/100)*n)-P;
+    return result;
 }
 
-long double emi(long double amount,int number_of_years, float rate_of_interest){
-    return amount * rate_of_interest * (1+rate_of_interest)*number_of_years/((1+rate_of_interest)*number_of_years-1);
+long double emi(long double P,int n, float r){
+    long double result = P*r*(1+r)*n/((1+r)*n-1);
+    return result;
 }
